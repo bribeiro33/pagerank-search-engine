2f096b00ff6f10bb45a33c8733c1fa08ddbd1640
Barbara Dreyer Rentz Ribeiro <53235549+bribeiro33@users.noreply.github.com>
2023-04-12 12:58:59 -0400

    Wrote map1 and reduce1

--- inverted_index/map1.py
+++ inverted_index/map1.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+"""Map 1."""
+import re
+import csv
+import sys
+
+# Stop words: list of stop words in inverted_index/stopwords.txt
+with open('stopwords.txt') as infile:
+    # strip and casefold redundant, as no extra whitspace or capitals
+    stop_words = set(infile.read().split('\n').strip().casefold())
+
+# 1. Data clean
+# 2. Organize terms to find all docs that contain a term in reduce
+csv.field_size_limit(sys.maxsize)
+for line in csv.reader(sys.stdin):
+    doc_id, doc_title, doc_body = line
+    # Combine both document title and document body by concatenating them, 
+    # separated by a space.
+    doc_text = doc_title + " " + doc_body
+    # Remove non-alphanumeric characters (that also aren’t spaces)
+    text = re.sub(r"[^a-zA-Z0-9 ]+", "", doc_text)
+    # Case insensitive. 
+    # Convert upper case characters to lower case using casefold().
+    text = text.casefold()
+    # Split the text into whitespace-delimited terms.
+    text_list = text.split()
+    # Remove stop words.
+    text_list = [word for word in text_list if word not in stop_words]
+    # If term, post_cleaning, is in doc --> <term, doc_id>
+    for term in text_list:
+        val_dict = {"doc_id": doc_id, "count": 1, "doc_text": text_list}
+        print(f"{term}\t{val_dict}")
--- inverted_index/reduce1.py
+++ inverted_index/reduce1.py
@@ -1,1 +1,57 @@
 #!/usr/bin/env python3
+"""Reduce 1.
+Find all docs that contain tk for all k
+and calc nk (num of docs containing tk)
+input: 
+grouped: by ti
+key: t1 val: {"doc_id": doc_id_a, "count": 1, "doc_text": doc_text_a}
+key: t1 val: {"doc_id": doc_id_b, "count": 1, "doc_text": doc_text_b}
+key: t2 val: {"doc_id": doc_id_a, "count": 1, "doc_text": doc_text_a}
+
+output:
+key: t1 val: {"N": N, "nk": n1, 
+              "docs": [(doc_id_a, doc_text_a), (doc_id_b, doc_text_b)]}
+key: t2 val: {"N": N, "nk": n2, "docs": [(doc_id_a, doc_text_b)]}
+
+"""
+import sys
+import itertools
+
+# Find all docs that contain ti for all i 
+# sorted docs by id
+
+with open("total_document_count.txt") as infile:
+    N = int(infile.read())
+
+
+def reduce_one_group(key, group):
+    """Reduce one group."""
+    group = list(group) #needed?
+    nk = 0
+    docs = []
+    for line in group:
+        # increase nk by count
+        value = line.split("\t")[1] #val should be a dict
+        # Add value to total_count (sould increment by one)
+        nk += value.count
+        # add doc_id and doc_text to docs
+        docs.append[(value.doc_id, value.doc_text)]
+
+    val_dict = {"N": N, "nk": nk, "docs": docs}
+    sys.stdout.write(f"{key}\n{val_dict}")
+
+
+
+def keyfunc(line):
+    """Return the key from a TAB-delimited key-value pair."""
+    return line.split()[0]
+
+
+def main():
+    """Divide sorted lines into groups that share a key."""
+    for key, group in itertools.groupby(sys.stdin, keyfunc):
+        reduce_one_group(key, group)
+
+
+if __name__ == "__main__":
+    main()
