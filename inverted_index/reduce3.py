83615b2006a7799f8ec62ee7ccb15764ba2ea91b
golpari <golpari@umich.edu>
2023-04-13 20:30:35 -0400

    reduce3 done, syntax wrong?

--- inverted_index/reduce3.py
+++ inverted_index/reduce3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Reduce 2.
+"""Reduce 3.
 Calc idfk = log(n/nk)
 input: 
 key: t1 val: {"N": N, "nk": n1, 
@@ -7,44 +7,59 @@ key: t1 val: {"N": N, "nk": n1,
 key: t2 val: {"N": N, "nk": n2, "docs": [doc_id_j, tfjk]}
 
 output: 
-key: t1 val: {"N": N, "idfk": idfk, "nk": n1, 
+key: t1 val: {"idfk": idfk, "nk": n1, 
               "docs": [doc_id_i, tfik]}
-key: t2 val: {"N": N, "idfk": idfk, "nk": n2,
+key: t2 val: {"idfk": idfk, "nk": n2,
               "docs": [doc_id_j, tfjk]}
 
-e.g. idk what this is this is from reduce2 and i copied stuff over
+e.g. of INPUT (i think? from reducer 2 and map3 passthrough?)
 d3js	{"N": 3, "nk": 1, "docs": ["1", 1]}
 document	{"N": 3, "nk": 4, "docs": ["1", 2]}
 document	{"N": 3, "nk": 4, "docs": ["2", 1]}
 document	{"N": 3, "nk": 4, "docs": ["3", 1]}
+
+e.g. of OUTPUT
+d3js	{"idfk": log(3/1), "nk": 1, "docs": ["1", 1]}
+//this next part i dont get, is this "docs" but expanded? but like its not.
+document	{"idfk": log(3/1), "nk": 4, "docs": ["1", 2]}
+document	{"idfk": log(3/1), "nk": 4, "docs": ["2", 1]}
+document	{"idfk": log(3/1), "nk": 4, "docs": ["3", 1]}
 """
 
 import sys
 import itertools
 import json
 import re
+import math
 
 # Calculate tfik
 
 def reduce_one_group(key, group):
     """Reduce one group."""
 
-    # TODO: mlorp fix me, this is from reducer 2,
-    # Each line is a term, file id pairing
+    # TODO: mlorp fix me
+    # Each line is a term, then the file info
     # The number of lines in the group = the number of times 
     # the term appears in the doc
-    key_correct = re.sub(r"[^a-zA-Z0-9 ]+", "", key)
-    term, doc_id = key_correct.split()
-    freq_count = 0
-    for line in group:
-        freq_count += 1
+    # log(a,Base)
+    file_count = 0
+    with open("total_document_count.txt", "r") as file:
+        file_count = float(file.readline())
 
-        # Just to keep curr info, pass on
-        value = line.split("\t")[1]
-        value_dict = json.loads(value)
-    # Edit docs to be a list of doc_id and term_freq
-    value_dict['docs'] = [doc_id, freq_count]
-    sys.stdout.write(f"{term}\t{json.dumps(value_dict)}\n")
+    # so copy over all the stuff
+    # not changing anything, but also calcing tfik
+    for line in sys.stdin:
+        doc = line.strip()
+        doc = line.split()
+        term = doc[0].strip()
+        nk = doc[1].strip()
+        docid = doc[2].strip()
+        tfik = doc[3].strip()
+        # length of sys.stdin list is # docs thatre containing the term
+        # cause each list item is for a doc that contains the term
+        idfk = math.log(file_count / len(list(sys.stdin)), 10)
+        sys.stdout.write(f"{term}\t{idfk} {nk} {docid} {tfik}\n")
+        # shld i be doing some value dict-y thing w json?
 
 
 
