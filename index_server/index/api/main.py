82b85fb954369f1b7c899f6dcdd4b56533a6f998
Barbara Dreyer Rentz Ribeiro <53235549+bribeiro33@users.noreply.github.com>
2023-04-15 13:08:28 -0400

    editing loads

--- index_server/index/api/main.py
+++ index_server/index/api/main.py
@@ -5,43 +5,60 @@ import re
 from flask import jsonify, request
 import index
 
-
-STOPWORDS_SET = set()
-PAGERANK_DICT = {}
-INVERTEDINDEX_DICT = {}
+# Globals: stopwords (set), pagerank (dict), inverted_index (dict)
 
 def load_index():
     """Load inverted index, pagerank, and stopwords into memory."""
     index_dir = pathlib.Path(__file__).parent.parent
-    read_stopwords(index_dir)
-    read_pagerank(index_dir)
-    read_inverted_index(index_dir)
+    load_stopwords(index_dir)
+    load_pagerank(index_dir)
+    load_inverted_index(index_dir)
 
 
-def read_stopwords(index_dir):
-    """Read the stopwords.txt file."""
+def load_stopwords(index_dir):
+    """Load the stopwords.txt file into stopwords."""
+    # Create global var to be accessed in calc funcs later
+    global stopwords
+    stopwords = set()
+    # Get correct path: /index/stopwords.txt
     stopwords_file = index_dir / "stopwords.txt"
-    with open(str(stopwords_file), 'r', encoding='utf-8') as stopfile:
-        for line in stopfile:
-            STOPWORDS_SET.add(line.strip())
+    with open(str(stopwords_file), 'r', encoding='utf-8') as infile:
+        for line in infile:
+            # incase there's extra whitespace, strip
+            new_word = line.strip()
+            stopwords.add(new_word)
 
 
-def read_pagerank(index_dir):
-    """Read the pagerank.out file."""
+def load_pagerank(index_dir):
+    """Load the pagerank.out file into pagerank."""
+    # Create global var to be accessed in calc funcs later
+    global pagerank
+    pagerank = {}
+    # Get correct path: /index/pagerank.out
     pagerank_file = index_dir / "pagerank.out"
-    with open(str(pagerank_file), 'r', encoding='utf-8') as pagerankfile:
-        for line in pagerankfile:
+    with open(str(pagerank_file), 'r', encoding='utf-8') as infile:
+        for line in infile:
             line = line.strip()
-            doc_id, score = line.split(",")
-            PAGERANK_DICT[doc_id] = float(score)
+            # create a data structure that maps doc ID to PageRank score
+            #e.g. 
+            # 3434750,0.0193215
+            # 20381,0.0104298
+            docid, score = line.split(",")
+            pagerank[docid] = float(score)
 
 
-def read_inverted_index(index_dir):
+def load_inverted_index(index_dir):
     """Read the inverted_index.txt file, based on the configured envvar."""
+    global inverted_index
+    inverted_index = {}
+    
+    # which index_path was set is the index file of inverted index to read from 
+    # e.g. inverted_index_0.txt
     index_file_config = index.app.config["INDEX_PATH"]
+    # /index/inverted_index/inverted_index_0|1|2.txt
     inverted_index_file = index_dir / "inverted_index" / index_file_config
-    with open(str(inverted_index_file), 'r', encoding='utf-8') as indexfile:
-        for line in indexfile:
+    with open(str(inverted_index_file), 'r', encoding='utf-8') as infile:
+        for line in infile:
             term_info_list = line.strip().split()
             term_name = term_info_list[0]
             idf_k = term_info_list[1]
@@ -59,4 +76,4 @@ def read_inverted_index(index_dir):
                 "idf_k": idf_k,
                 "term_info_appear": term_appears
             }
-            INVERTEDINDEX_DICT[term_name] = term_context
+            inverted_index[term_name] = term_context
